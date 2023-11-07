a502a2311a3f1b379bfb4121fa7dc315d1917985
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2023-11-07 12:54:12 +0600

    Update setter_getter.dart

--- Encapsulation/setter_getter.dart
+++ Encapsulation/setter_getter.dart
@@ -1,6 +1,6 @@
 class MyClass {
-  final int _stdId = 3322; //global variable
-  late int _phoneNum; //global variable
+  final int _stdId = 3322; //private variable
+  late int _phoneNum; //private variable
 
   List<String> stdName(String name1, String name2, String name3) {
     List<String> myNameList = [name1, name2, name3]; //local variable
